1104c33708415ec57ac2b141525b4f39a31c19a2
Harshit Bhatia <harshitbhatia28698@gmail.com>
2022-03-06 00:53:05 +0530

    Added UserProfileService

---
+++ src/main/java/com/springbootapp/awsimageupload/service/UserProfileService.java
@@ -0,0 +1,103 @@
+package com.springbootapp.awsimageupload.service;
+
+import com.springbootapp.awsimageupload.adapter.S3Adapter;
+import com.springbootapp.awsimageupload.bucket.BucketName;
+import com.springbootapp.awsimageupload.model.UserProfile;
+import com.springbootapp.awsimageupload.repository.UserProfileDataAccessRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.FileStore;
+import java.util.*;
+
+import static org.apache.http.entity.ContentType.*;
+
+@Service
+public class UserProfileService {
+
+    private final UserProfileDataAccessRepository userProfileDataAccessRepository;
+    private final S3Adapter s3Adapter;
+
+    @Autowired
+    public UserProfileService(UserProfileDataAccessRepository userProfileDataAccessRepository,
+                              S3Adapter s3Adapter) {
+        this.userProfileDataAccessRepository = userProfileDataAccessRepository;
+        this.s3Adapter = s3Adapter;
+    }
+
+    public List<UserProfile> getUserProfiles() {
+        return userProfileDataAccessRepository.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        // 1. Check if image is not empty
+        isFileEmpty(file);
+        // 2. If file is an image
+        isImage(file);
+
+        // 3. The user exists in our database
+        UserProfile user = getUserProfileOrThrow(userProfileId);
+
+        // 4. Grab some metadata from file if any
+        Map<String, String> metadata = extractMetadata(file);
+
+        // 5. Store the image in s3 and update database (userProfileImageLink) with s3 image link
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
+        String filename = String.format("%s-%s", file.getOriginalFilename(), UUID.randomUUID());
+
+        try {
+            s3Adapter.save(path, filename, Optional.of(metadata), file.getInputStream());
+            user.setUserProfileImageLink(filename);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+
+    public byte[] downloadUserProfileImage(UUID userProfileId) {
+        UserProfile user = getUserProfileOrThrow(userProfileId);
+
+        String path = String.format("%s/%s",
+                BucketName.PROFILE_IMAGE.getBucketName(),
+                user.getUserProfileId());
+
+        return user.getUserProfileImageLink()
+                .map(key -> s3Adapter.download(path, key))
+                .orElse(new byte[0]);
+
+    }
+
+    private Map<String, String> extractMetadata(MultipartFile file) {
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+        return metadata;
+    }
+
+    private UserProfile getUserProfileOrThrow(UUID userProfileId) {
+        return userProfileDataAccessRepository
+                .getUserProfiles()
+                .stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(String.format("User profile %s not found", userProfileId)));
+    }
+
+    private void isImage(MultipartFile file) {
+        if (!Arrays.asList(
+                IMAGE_JPEG.getMimeType(),
+                IMAGE_PNG.getMimeType(),
+                IMAGE_GIF.getMimeType()).contains(file.getContentType())) {
+            throw new IllegalStateException("File must be an image [" + file.getContentType() + "]");
+        }
+    }
+
+    private void isFileEmpty(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new IllegalStateException("Cannot upload empty file [ " + file.getSize() + "]");
+        }
+    }
+
+}
